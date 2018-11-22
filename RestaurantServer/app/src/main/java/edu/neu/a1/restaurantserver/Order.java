421f143ce5b73225e76bfaa1503e90b5cd51b2dc
Yue Zheng <zheng.yue1@husky.neu.edu>
2018-11-22 15:36:50 -0500

    Item, Order, OrderList class

---
+++ RestaurantServer/app/src/main/java/edu/neu/a1/restaurantserver/Order.java
@@ -0,0 +1,45 @@
+package edu.neu.a1.restaurantserver;
+
+public class Order {
+
+    private int customerId;
+    private String customerName;
+    private Item item;
+    private String status;
+
+    public Order() {
+
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
