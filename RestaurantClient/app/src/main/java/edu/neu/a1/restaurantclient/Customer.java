82bb459b588cec2b228964638c931711ec90abaa
Andong Wang <wang.ando@husky.neu.edu>
2018-11-25 12:43:42 -0500

    Update costomer

--- RestaurantClient/app/src/main/java/edu/neu/a1/restaurantclient/Customer.java
+++ RestaurantClient/app/src/main/java/edu/neu/a1/restaurantclient/Customer.java
@@ -1,6 +1,33 @@
 package edu.neu.a1.restaurantclient;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
 
     // TODO: add attributes and methods
+    String name;
+    int id;
+    List orderList = new ArrayList<Order>();
+
+    public String getname(){
+        return name;
+    }
+
+    public void setname(String name){
+        this.name = name;
+    }
+
+    public int getid(){
+        return id;
+    }
+
+    public void setid(int id){
+        this.id = id;
+    }
+
+    public List addOrder(Order order){
+        orderList.add(order);
+        return orderList;
+    }
 }
