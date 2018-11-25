90d5bf4a1e562a8bd29957a5ed05cb550b173074
Yue Zheng <zheng.yue1@husky.neu.edu>
2018-11-25 16:45:00 -0500

    layouts + styles + activity frame

--- RestaurantServer/app/src/main/java/edu/neu/a1/restaurantserver/MainActivity.java
+++ RestaurantServer/app/src/main/java/edu/neu/a1/restaurantserver/MainActivity.java
@@ -1,13 +1,59 @@
 package edu.neu.a1.restaurantserver;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main1);
+        addListenerOnButton();
+        addListenerOnButton2();
+    }
+
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, OrderDetailsActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
+    }
+        public void addListenerOnButton2() {
+
+            final Context context = this;
+
+            button = (Button) findViewById(R.id.button2);
+
+            button.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View arg0) {
+
+                    Intent intent = new Intent(context, OrderDetailsActivity.class);
+                    startActivity(intent);
+
+                }
+
+            });
+
     }
 }
---
+++ RestaurantServer/app/src/main/java/edu/neu/a1/restaurantserver/OrderDetailsActivity.java
@@ -0,0 +1,71 @@
+package edu.neu.a1.restaurantserver;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class OrderDetailsActivity extends Activity{
+
+    TextView msg, orderId, burger, chicken, status, fries, onionrings;
+    Button confirm, cancelorder, back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        burger = (TextView)findViewById(R.id.burger);
+        msg = (TextView)findViewById(R.id.msg);
+        orderId = (TextView)findViewById(R.id.orderId);
+        chicken = (TextView)findViewById(R.id.chicken);
+        fries = (TextView)findViewById(R.id.fries);
+        onionrings = (TextView)findViewById(R.id.onionrings);
+        status = (TextView)findViewById(R.id.status);
+        confirm = (Button)findViewById(R.id.confirm);
+        cancelorder = (Button)findViewById(R.id.cancel);
+
+
+        addListenerOnConfirm();
+        addListenerOnBack();
+    }
+
+    public void addListenerOnConfirm(){
+
+        final Context context = this;
+
+        confirm = (Button) findViewById(R.id.confirm);
+
+//        confirm.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View arg0) {
+//
+//                Intent intent = new Intent(context, MainActivity.class);
+//                startActivity(intent);
+//
+//            }
+//
+//        });
+    }
+
+    public void addListenerOnBack(){
+
+        final Context context = this;
+
+        back = (Button) findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, MainActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
+    }
+}
